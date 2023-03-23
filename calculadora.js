635a9baadfbd6b3a217c50bd30f03e28531b08a7
frankarmond <126296453+frankarmond@users.noreply.github.com>
2023-03-23 18:18:09 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,29 @@
+var numero1 = ""
+var estaAdd = false
+
 function digita_campo(numero_clicado){
-    var campo = document.getElementsByTagName('input')[0]
-    campo.value = campo.value + numero_clicado
+    var valorCampo = document.getElementsByTagName('input')[0]
+ 
+    if (estaAdd){
+        numero1 = valorCampo.value
+        valorCampo.value = numero_clicado
+        estaAdd = false
+   } else{
+        valorCampo.value = valorCampo.value + numero_clicado
+   }
 }
 
 function limpar(){
-    var campo = document.getElementsByTagName('input')[0]
-    campo.value = ""
+    var valorCampo = document.getElementsByTagName('input')[0]
+    valorCampo.value = ""
+}
+
+function resultado(){
+    var valorCampo = document.getElementsByTagName('input')[0]
+    var resultado = parseFloat(numero1) + parseFloat(valorCampo.value)
+    valorCampo.value = resultado
+}
+
+function botaoSoma(){
+    estaAdd = true
 }
